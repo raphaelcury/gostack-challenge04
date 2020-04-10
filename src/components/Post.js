cd917275bb07c609320efee4c717504592dde660
Raphael Cury <raphaelcury@MacBook-Pro-de-Raphael.local>
2020-04-10 16:12:56 -0300

    Content and formatting in Post.

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+
+import avatar from '../img/avatar.png';
 
 function Post() {
   return (
     <li className="post">
-      Post
+      <div className="postUser">
+        <img src={avatar} alt="Avatar"/>
+        <strong >Raphael Cury <span>04 Jun 2019</span></strong>
+      </div>
+      <p>Pessoal, alguém sabe quando acaba a quarentena?</p>
+      <ul className="comments">
+        <li>Comentário 1</li>
+        <li>Comentário 2</li>
+        <li>Comentário 3</li>
+        <li>Comentário 4</li>
+      </ul>
     </li>
   );
 }
