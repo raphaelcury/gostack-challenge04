f77d12a1336f11c4c0cefcbb02a7ba7ba272daf7
Raphael Cury <raphaelcury@MacBook-Pro-de-Raphael.local>
2020-04-10 15:36:02 -0300

    First version of Post component.

---
+++ src/components/Post.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Post() {
+  return (
+    <li className="post">
+      Post
+    </li>
+  );
+}
+
+export default Post;
--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 
+import Post from './Post';
+
 class PostList extends Component {
   render() {
     return(
-      <div className="postList">
-        <ul>
-          <li>Post 1</li>
-          <li>Post 2</li>
-          <li>Post 3</li>
-          <li>Post 4</li>
+      <div className="postListDiv">
+        <ul className="postList">
+          <Post />
+          <Post />
+          <Post />
         </ul>
       </div>
     );
