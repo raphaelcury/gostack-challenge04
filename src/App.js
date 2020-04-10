39cc0c484eea0b68849a64f2a7ac1f408695c1b9
Raphael Cury <raphaelcury@MacBook-Pro-de-Raphael.local>
2020-04-10 14:40:45 -0300

    Header component

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ import Header from './components/Header';
 
 function App() {
   return(
-    <Header />
+    <div className="main">
+      <Header />
+    </div>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
+
+import facebookLogo from '../img/facebook.png';
+import userIcon from '../img/user.png';
 
 function Header() {
   return(
-    <div className="header">Facebook</div>
+    <div className="header">
+      <img id="facebook" src={facebookLogo} alt="Facebook"/>
+      <div className="profile">
+        <a href="">
+          <strong>Meu perfil</strong>
+          <img id="user" src={userIcon} alt="User"/>
+        </a>
+      </div>
+    </div>
   );
 }
 
